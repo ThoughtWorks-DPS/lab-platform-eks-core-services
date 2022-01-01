d6ae57d3dbb4cdd84bf3061ebaec18d5cda0fbd1
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-01-01 12:53:16 -0600

    [ncheneweth] test metrics apis deployment

--- scripts/deploy_metrics_server.sh
+++ scripts/deploy_metrics_server.sh
@@ -3,4 +3,4 @@ export CLUSTER=$1
 export CHART_VERSION=$(cat $CLUSTER.auto.tfvars.json | jq -r .metrics_server_chart_vevrsion)
 
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
-helm upgrade --version $CHART_VERSION --install metrics-server metrics-server/metrics-server --values metrics-apis/metrics-server-values.yaml
+helm upgrade --version $CHART_VERSION --namespace kube-system --install metrics-server metrics-server/metrics-server --values metrics-apis/metrics-server-values.yaml
