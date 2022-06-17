65264bad2d1bcae8ff06837b3d156f6fb4c1205f
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-06-17 14:42:06 -0500

    [ncheneweth] refresh to current versions of core services. Note- efscsi not functioning
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/validate_core_services.sh
+++ scripts/validate_core_services.sh
@@ -5,11 +5,11 @@ export CLUSTER=$1
 export AWS_ACCOUNT_ID=$(cat $CLUSTER.auto.tfvars.json | jq -r .aws_account_id)
 export AWS_DEFAULT_REGION=$(cat $CLUSTER.auto.tfvars.json | jq -r .aws_region)
 export AWS_ASSUME_ROLE=$(cat $CLUSTER.auto.tfvars.json | jq -r .aws_assume_role)
-aws sts assume-role --output json --role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/$AWS_ASSUME_ROLE --role-session-name lab-platform-eks-core-services > credentials
+# aws sts assume-role --output json --role-arn arn:aws:iam::$AWS_ACCOUNT_ID:role/$AWS_ASSUME_ROLE --role-session-name lab-platform-eks-core-services > credentials
 
-export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
-export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
-export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
+# export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
+# export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
+# export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
 
 export DESIRED_METRICS_SERVER_VERSION=$(cat $CLUSTER.auto.tfvars.json | jq -r .metrics_server_version)
 
